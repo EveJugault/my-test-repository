1d4e9e50bd71c2a4b3f1ffbfba60c4040ff8bbf3
JUGAULT <eve.jugault@atos.net>
2022-02-28 11:36:23 +0100

    ajout des fichiers au dépôt

---
+++ project-apache-camel/src/main/java/org/maven/App.java
@@ -0,0 +1,38 @@
+package org.maven;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) throws IOException
+    {
+        /**System.out.println( "Hello World!" );
+        
+        Properties vProps = new Properties();
+        InputStream vInputStream = null;
+        try{
+        	vInputStream = App.class.getResourceAsStream("/info.properties");
+        	vProps.load(vInputStream);
+        }
+        finally{
+        	if(vInputStream != null) {
+        		vInputStream.close();
+        	}
+        }
+        System.out.println( "Application version : " + vProps.getProperty("org.maven.version", "?"));
+        */
+    }
+}
+
+
